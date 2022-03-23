9402e2bec27c6cdb9569e71eb7fefe725875ce2b
Ali Baloch <ali.albaloshi2000@gmail.com>
2022-03-23 17:10:07 +0500

    Game Donee

--- GuessingGame/app/src/main/java/com/example/guessinggame/MainActivity.kt
+++ GuessingGame/app/src/main/java/com/example/guessinggame/MainActivity.kt
@@ -3,19 +3,36 @@ package com.example.guessinggame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
 
+    val random = Random()
+    var numtoGuess = random.nextInt(1000)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     fun onGuess(view : View){
         var textview = findViewById<TextView>(R.id.textView2)
+        var text = findViewById<EditText>(R.id.editTextNumber)
+        val num = Integer.parseInt(text.text.toString())
+
+        if (numtoGuess > num)
+            textview.text = "You guess is too low"
+        else if (numtoGuess < num)
+            textview.text = "Your guess is to high"
+        else
+            textview.text = "Yes! You guessed it right :)"
+
+
+
+
     }
 }

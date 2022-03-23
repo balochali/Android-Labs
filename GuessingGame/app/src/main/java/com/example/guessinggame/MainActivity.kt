982dd80f7861035a28f110ad7be36f134aaa9d6e
Ali Baloch <ali.albaloshi2000@gmail.com>
2022-03-23 16:46:24 +0500

    Design Done

--- GuessingGame/app/src/main/java/com/example/guessinggame/MainActivity.kt
+++ GuessingGame/app/src/main/java/com/example/guessinggame/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.guessinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onGuess(view : View){
+        var textview = findViewById<TextView>(R.id.textView2)
     }
 }

0f527b6e456626666916085562bcbf419564ad9a
Ali Baloch <ali.albaloshi2000@gmail.com>
2022-05-12 23:21:46 +0500

    Lab 2 Task 1

---
+++ Lab2Task1/app/src/main/java/com/example/lab2task1/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.lab2task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("called", "On Start")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("called", "On Resume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("called", "On Pause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("called", "On Stop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("called", "On Restart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("called", "On Destroy")
+    }
+}

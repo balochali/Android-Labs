49ce26af88a06e3e2929f3f49c1b736ec92f0828
Ali Baloch <ali.albaloshi2000@gmail.com>
2022-05-22 13:24:01 +0500

    Media Player

--- ExamPractice/app/src/main/java/com/example/exampractice/MainActivity.kt
+++ ExamPractice/app/src/main/java/com/example/exampractice/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.exampractice
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+
+    fun play (v : View){
+        var mediaPlayer = MediaPlayer.create(this, R.raw.pir)
+        mediaPlayer.start()
+    }
+    fun stop (v : View){
+        var mediaPlayer = MediaPlayer.create(this, R.raw.pir)
+        mediaPlayer.stop()
     }
 }

e9e6f9bb0bf05ece451b1552372a4f68e59a92b2
Ali Baloch <ali.albaloshi2000@gmail.com>
2022-06-23 12:24:20 +0500

    Registration Activity DOne

--- EasyBus/app/src/main/java/com/example/easybus/RegistrationActivity.kt
+++ EasyBus/app/src/main/java/com/example/easybus/RegistrationActivity.kt
@@ -4,17 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
-class LoginActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_registration)
 
         supportActionBar?.hide()
+
     }
 
-    fun SignUP(v : View){
-        val intent = Intent(this, RegistrationActivity::class.java)
+
+    fun SignIN(v : View){
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
 }
